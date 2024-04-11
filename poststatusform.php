94cd04e6416c90ac8e8a421b77d945db226adb3a
shaneel <shaneelkhatri@gmail.com>
2024-04-11 17:26:48 +1200

    status

--- poststatusform.php
+++ poststatusform.php
@@ -11,7 +11,6 @@
         <label for="stcode">Status Code:</label><br>
         <input type="text" name="stcode" id="stcode" required
         pattern="[S][0-9]{4}" maxlength="5" value=""><br>
-        <i>The code must start with an uppercase letter "S" followed by 4 digits e.g. S0001 .</i><br><br>
         <!-- Add other required form elements here -->
         <input type="submit" value="Submit">
     </form>
