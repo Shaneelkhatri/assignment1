b6380b484f7068b330d335e223055db28c19953c
shaneel <shaneelkhatri@gmail.com>
2024-04-11 18:04:56 +1200

    new

--- poststatusform.php
+++ poststatusform.php
@@ -6,7 +6,7 @@
     <title>Post Status Page</title>
 </head>
 <body>
-    <h1>Post Status Form</h1>
+    <h1>Status Post System</h1>
     <form>
         <label for="stcode">Status Code:</label><br>
         <input type="text" name="stcode" id="stcode" required
@@ -16,9 +16,18 @@
         <label for="status">Status:<label><br>
         <input type="text" name = "st"
         required pattern ="[a-zA-Z0-9,!?. ]*[a-zA-Z0-9!?.]+[a-zA-Z0-9,!?. ]* "value="<?php echo isset($_POST['st']) ? htmlspecialchars($_POST['st']) : ''; ?>"><br>
-        
+        <br>
+        <label for="share">Share:</label>
+        <input type="radio" id="share1" name="share" value="University" required>
+        <label for="share1">University</label>
+        <input type="radio" id="share2" name="share" value="Class">
+        <label for="share2">Class</label>
+        <input type="radio" id="share3" name="share" value="Private">
+        <label for="share3">Private</label>
 
-        <input type="submit" value="Submit">
+        <label for ="date">Date:</label>
+        <input type ="date" name ="date" required value ="<?php echo date('d-m-y'); ?>"><br>
+
     </form>
 </body>
 </html>
